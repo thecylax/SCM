d196951eb6f3887f05fe4b74fac95005d88ebc09
Robson Cardoso dos Santos <cardoso.rcs@gmail.com>
2013-05-20 22:16:18 -0300

    New color on settings.py

--- GUI/settings.py
+++ GUI/settings.py
@@ -1,9 +1,16 @@
 # color scheme: http://www.perbang.dk/rgb/1C001C/
+import pygame
+
+FONT = 'freesansbold.ttf'
+FONT_SIZE = 14
 
 BLACK = (  0,   0,   0)
 WHITE = (255, 255, 255)
 BLUE =  (  0,   0, 255)
 GREEN = (  0, 255,   0)
 RED =   (255,   0,   0)
-MAGENTA_WIDGET = (76, 0, 76)
+MAGENTA_FG = (76, 0, 76)
 MAGENTA_BG = (51, 0, 51)
+DARKGRAY  = ( 64,  64,  64)
+GRAY      = (128, 128, 128)
+LIGHTGRAY = (212, 208, 200)
